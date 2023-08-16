0c1b1edcfe51bbcc28d5f46fbd03ba708c02e8f3
kubealex <al.rossi87@gmail.com>
2023-08-16 15:09:04 +0200

    cleanup

--- src/main/java/it/kubealex/ansible/controller/service/AAPControllerService.java
+++ src/main/java/it/kubealex/ansible/controller/service/AAPControllerService.java
@@ -3,11 +3,9 @@ package it.kubealex.ansible.controller.service;
 import java.util.Base64;
 
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
